04b3053b1e801153bdac0afb932d885bbcc2b97c
davidgm05 <dapnestic05@gmail.com>
2023-05-11 10:51:40 +0200

    subida

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 ﻿﻿Console.WriteLine("a continuación podrás escribir dos números para poderlos sumar, restar, multiplicar o dividir");
 Console.WriteLine("escriba el primer número");
-var primerNumero = Console.ReadLine();
-int numero1 = int.Parse(primernum);
+var primernum = Console.ReadLine();
+int num1 = int.Parse(primernum);
 Console.WriteLine("escriba el seguno número");
-var segundoNumero = Console.ReadLine();
-int numero2 = int.Parse(segundonum);
-Suma(numero1,numero2);
+var segundonum = Console.ReadLine();
+int num2 = int.Parse(segundonum);
+Suma(num1,num2);
 static int Suma(int suma1, int suma2)
 {
     int resultado = suma1 + suma2;
@@ -13,10 +13,10 @@ static int Suma(int suma1, int suma2)
     return resultado;
 }
 Resta(num1,num2);
-static int Resta(int minuendo, int substraendo)
+static int Resta(int minuendo, int sustraendo)
 {
-    int resultado = minuendo - substraendo;
-    Console.WriteLine("la resta de  " + minuendo + " y " + substraendo + " es: " + resultado);
+    int resultado = minuendo - sustraendo;
+    Console.WriteLine("la resta de  " + minuendo + " y " + sustraendo + " es: " + resultado);
     return resultado;
 }
 Producto(num1,num2);
